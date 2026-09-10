e63806c398866bc78feaddbb0ec554b6a5791d0e
akunbarukuyangini <akunbarukuyangini47@gmail.com>
2026-09-11 00:59:15 +0700

    Update data.js

--- data.js
+++ data.js
@@ -6,31 +6,44 @@
 // ============================================================
 const data = {
   unit: "SPPG Ciparay Dua — Majalengka",
-  date: "Selasa, 8 September 2026",
-  menuTitleHTML: "<span>Chiken Nugget</span>",
+  date: "Senin, 14 September 2026",
+  menuTitleHTML: "<span>Ayam Kemangi</span>",
   items: [
     {name:"Nasi Putih", price:"Rp.950"},
-    {name:"Chiken Nugget", price:"Rp.4.155"},
-    {name:"Tempe Goreng", price:"Rp.755"},
-    {name:"Tumis Brokoli Wortel", price:"Rp.1.970"},
-    {name:"Jeruk", price:"Rp.2.295"},
+    {name:"Ayam Kemangi", price:"Rp.4.430"},
+    {name:"Tahu Goreng", price:"Rp.775"},
+    {name:"Tumis Labu Siam Wortel", price:"Rp.1.550"},
+    {name:"Pisang Cavendish", price:"Rp.2.295"},
   ],
   portions: {
     besar: {
       photo: "foto-porsi-besar.jpg",
-      nutrition:{karbohidrat:"84.12 g", protein:"20.27 g", lemak:"21.67 g", serat:"2.56 g", energi:"607.5 kkal"}
+      nutrition:{karbohidrat:"80.14 g", protein:"21.62 g", lemak:"25.43 g", serat:"4.17 g", energi:"635.5 kkal"}
     },
     kecil: {
       photo: "foto-porsi-kecil.jpg",
-      nutrition:{karbohidrat:"49.08 g", protein:"17.57 g", lemak:"17.42 g", serat:"1.78 g", energi:"422.2 kkal"}
+      nutrition:{karbohidrat:"59.82 g", protein:"16.63 g", lemak:"21.55 g", serat:"3.75 g", energi:"497.2 kkal"}
     }
   },
   totalPorsi: "3.007 Porsi",
   recipients: [
-    "TPQ Al-Karim","RA Sabilul Chalim","RA Himatul Yaqien","RA Al-Hidayah",
-    "TK Al-Ishlah","TK Tunas Mekar","KB Tunas Mekar","TK Nurul Huda",
-    "RA Raudlatul Ulum","SDN Leuwimunding 4","SDN Leuwimunding 1","SDN Parungjaya 1",
-    "SDN Parungjaya 2","SMPN 3 Leuwimunding","MA Nurul Iman","SLB YP Al-Azhar Leuwimunding",
-    "MTS Unggulan Raudlatul Mubtadin","3B Leuwimunding"
+      "TPQ Al-Karim",
+      "RA Sabilul Chalim",
+      "RA Himatul Yaqien",
+      "RA Al-Hidayah",
+      "TK Al-Ishlah",
+      "TK Tunas Mekar",
+      "KB Tunas Mekar",
+      "TK Nurul Huda",
+      "RA Raudlatul Ulum",
+      "SDN Leuwimunding 4",
+      "SDN Leuwimunding 1",
+      "SDN Parungjaya 1",
+      "SDN Parungjaya 2",
+      "SMPN 3 Leuwimunding",
+      "MA Nurul Iman",
+      "SLB YP Al-Azhar Leuwimunding",
+      "MTS Unggulan Raudlatul Mubtadin",
+      "3B Leuwimunding"
   ]
 };
